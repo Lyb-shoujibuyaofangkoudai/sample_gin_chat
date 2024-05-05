0c2eedd51a223585cd6b347f4372d29b2b568545
yongbin <1103782715@qq.com>
2024-05-05 09:26:46 +0800

    日志新增写入文件

--- core/logrus.go
+++ core/logrus.go
@@ -19,6 +19,31 @@ const (
 
 type LogFormatter struct{}
 
+func (*LogFormatter) printLog(entry *logrus.Entry, b *bytes.Buffer, timestamp, logPrefix string, levelColor int) {
+	env := viper.GetString("system.env")
+	if env == "debug" {
+		if entry.HasCaller() {
+			//	 自定义文件路径
+			funcVal := entry.Caller.Function
+			fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
+			//	 自定义输出格式
+			fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s %s %s \n", logPrefix, timestamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
+		} else {
+			fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s \n", logPrefix, timestamp, levelColor, entry.Level, entry.Message)
+		}
+	} else if env == "release" {
+		if entry.HasCaller() {
+			//	 自定义文件路径
+			funcVal := entry.Caller.Function
+			fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
+			//	 自定义输出格式
+			fmt.Fprintf(b, "%s[%s] [%s] [%s] [%s] %s \n", logPrefix, timestamp, entry.Level, fileVal, funcVal, entry.Message)
+		} else {
+			fmt.Fprintf(b, "%s[%s] [%s]  %s \n", logPrefix, timestamp, entry.Level, entry.Message)
+		}
+	}
+}
+
 func (t *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	var levelColor int
 	switch entry.Level {
@@ -41,16 +66,17 @@ func (t *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 	logPrefix := viper.GetString("logger.prefix")
 	timestamp := entry.Time.Format("2006-01-02 15:04:05")
-	if entry.HasCaller() {
-		//	 自定义文件路径
-		funcVal := entry.Caller.Function
-		fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
-
-		//	 自定义输出格式
-		fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s %s %s \n", logPrefix, timestamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
-	} else {
-		fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s \n", logPrefix, timestamp, levelColor, entry.Level, entry.Message)
-	}
+	t.printLog(entry, b, timestamp, logPrefix, levelColor)
+	//if entry.HasCaller() {
+	//	//	 自定义文件路径
+	//	funcVal := entry.Caller.Function
+	//	fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
+	//
+	//	//	 自定义输出格式
+	//	fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s %s %s \n", logPrefix, timestamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
+	//} else {
+	//	fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s \n", logPrefix, timestamp, levelColor, entry.Level, entry.Message)
+	//}
 	return b.Bytes(), nil
 }
 
@@ -65,7 +91,10 @@ func InitLogger() *logrus.Logger {
 	}
 	mLog.SetLevel(level) // 设置最低日志级别
 	InitDefaultLogger()
-
+	if viper.GetString("system.env") == "release" {
+		// 生产环境就写入日志到文件
+		LogWriteToFile(mLog)
+	}
 	return mLog
 }
 
@@ -81,6 +110,17 @@ func InitDefaultLogger() {
 	logrus.SetLevel(level) // 设置最低日志级别
 }
 
+func LogWriteToFile(l *logrus.Logger) {
+	// todo 写入日志待完善，没有校验文件是否存在，日志分割等操作
+	file, err := os.OpenFile("log/logrus.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err == nil {
+		l.Out = file
+	} else {
+		l.Info("Failed to log to file, using default stderr")
+	}
+}
+
+// LogrusGormLogger gorm日志记录器
 type LogrusGormLogger struct {
 	*logrus.Logger
 }
